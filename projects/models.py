c3451fb0b8152d5e856c40560e081e877b1a341a
Ahmad-A-AlTamim <ahmadabedtamim@gmail.com>
2024-08-14 23:12:32 +0300

    add project model and store projects data , and make tecnology field max lenght from 20 to 150 char

--- projects/models.py
+++ projects/models.py
@@ -1,3 +1,12 @@
 from django.db import models
-
+from mainApp.models import account
 # Create your models here.
+class Project(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    technology = models.CharField(max_length=150)
+    user = models.ForeignKey(account, on_delete=models.CASCADE)
+    image = models.ImageField(upload_to="images/projects/%Y/%m/%d/%H/%M/%S")
+
+    def __str__(self):
+        return self.title
