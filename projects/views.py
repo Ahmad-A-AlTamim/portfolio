a14bb856137d82341d157a49358106ab522b1fca
Ahmad-A-AlTamim <ahmadabedtamim@gmail.com>
2024-08-19 00:54:36 +0300

    add project page and show project details and fix all responsive problems and finished project

--- projects/views.py
+++ projects/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
+from .models import Project
 def project(request):
-
     return render(request, 'pages/projects.html')
+
+def projectDetail(request, title):
+    project=Project.objects.get(title=title)
+    context = {
+        'project':project,
+        'tech':project.technology.split(',')
+    }
+    return render(request, 'pages/projectPage.html',context)
 # Create your views here.
